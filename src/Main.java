193fb0cc616856e7d774e54006e8fa5d4b90791e
zseszti0 <zseszti42.3@gmail.com>
2025-09-29 23:44:22 +0200

    fistekfkekfe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -7,8 +8,8 @@ import java.util.Scanner;
 public class Main {
     protected File wd = new File(System.getProperty("user.dir"));
 
-    protected void pwd(){
-        System.out.println(wd.getName() + " " + wd.listFiles().length);
+    protected void pwd() throws IOException {
+        System.out.println(wd.getCanonicalPath() + " " + wd.listFiles().length);
     }
     protected void ls(String arg){
         List<File> files = List.of(wd.listFiles());
@@ -68,10 +69,82 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+    }
+    protected void cat(String arg){
+        try (BufferedReader reader = new BufferedReader(new FileReader(arg))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void length(String arg){
+        File sourceFile = new File(wd,arg);
+        System.out.println(sourceFile.length());
+    }
+    protected void head(String arg,int n){
+        try (BufferedReader reader = new BufferedReader(new FileReader(arg))) {
+            String line;
+            for(int i = 0; i < n;i++){
+                if((line = reader.readLine()) != null){
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void tail(String arg,int n){
+        try (BufferedReader reader = new BufferedReader(new FileReader(arg))) {
+            String line;
+            List<String> lines = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            for(int i = lines.size()-n; i < lines.size(); i++){
+                System.out.println(lines.get(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void wc(String arg){
+        try (BufferedReader reader = new BufferedReader(new FileReader(arg))) {
+            String line;
+            int linesC = 0;
+            int wordsC = 0;
+            int lettersC = 0;
+            while ((line = reader.readLine()) != null) {
+                linesC++;
+                for(String split : line.split(" ")){
+                    if(!split.isEmpty()){
+                        wordsC++;
+                    }
+                }
+                lettersC += line.length();
+            }
+            System.out.println(linesC + " " + wordsC + " " + lettersC);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    protected void grep(String arg1,String arg2){
+        try (BufferedReader reader = new BufferedReader(new FileReader(arg2))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if(line.matches(arg1)){
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public  void main(String[] args) {
+
+    public  void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String line;
         while (!(line = scanner.nextLine()).equals("exit")) {
@@ -96,7 +169,31 @@ public class Main {
                     mv(commands.get(1),commands.get(2));
                     break;
                 case "cp":
-                    mv(commands.get(1),commands.get(2));
+                    cp(commands.get(1),commands.get(2));
+                    break;
+                case "cat":
+                    cat(commands.get(1));
+                    break;
+                case "length":
+                    length(commands.get(1));
+                    break;
+                case "head":
+                    if(commands.size() > 2)
+                        head(commands.get(1),Integer.parseInt(commands.get(2)));
+                    else
+                        head(commands.get(1),10);
+                    break;
+                case "tail":
+                    if(commands.size() > 2)
+                        tail(commands.get(1),Integer.parseInt(commands.get(2)));
+                    else
+                        tail(commands.get(1),10);
+                    break;
+                case "wc":
+                    wc(commands.get(1));
+                    break;
+                case "grep":
+                    grep(commands.get(1),commands.get(2));
                     break;
             }
 
